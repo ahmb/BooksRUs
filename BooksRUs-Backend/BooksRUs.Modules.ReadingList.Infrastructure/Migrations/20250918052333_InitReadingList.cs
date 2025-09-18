02eb18bcd5d7a70733ec1783953d43ccf49aeac3
arb <raufguy@Gmail.com>
2025-09-18 01:55:56 -0400

    Added docker support and added DB migrations

---
+++ BooksRUs-Backend/BooksRUs.Modules.ReadingList.Infrastructure/Migrations/20250918052333_InitReadingList.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BooksRUs.Modules.ReadingList.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitReadingList : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "readinglist");
+
+            migrationBuilder.CreateTable(
+                name: "Items",
+                schema: "readinglist",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    BookId = table.Column<Guid>(type: "uuid", nullable: false),
+                    AddedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_UserId_BookId",
+                schema: "readinglist",
+                table: "Items",
+                columns: new[] { "UserId", "BookId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Items",
+                schema: "readinglist");
+        }
+    }
+}
